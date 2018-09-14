63f933166b0fca96f17a5a34c13c9ceda153d4b6
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2018-09-14 23:25:51 +0200

    Added Restore Capabilities

--- backup.runner/Program.cs
+++ backup.runner/Program.cs
@@ -13,47 +13,94 @@ namespace backup.runner
             Console.WriteLine("Backup Client for Azure Storage Account Tables");
             Console.WriteLine("");
 
-            // Check the parameter 
+            // Check the global parameters 
             ValidateParameter("SRC_ACCOUNT_NAME");
             ValidateParameter("SRC_ACCOUNT_KEY");
             ValidateParameter("TGT_ACCOUNT_KEY");
             ValidateParameter("TGT_ACCOUNT_KEY");
-            ValidateParameter("TGT_ACCOUNT_CONTAINER");
 
+            // Load the global parameters
             var srcAccountName = Environment.GetEnvironmentVariable("SRC_ACCOUNT_NAME");
             var srcAccountKey = Environment.GetEnvironmentVariable("SRC_ACCOUNT_KEY");
             var srcAccountEndpointSuffix = Environment.GetEnvironmentVariable("SRC_ACCOUNT_ENDPOINT_SUFFIX");
 
             var tgtAccountName = Environment.GetEnvironmentVariable("TGT_ACCOUNT_NAME");
             var tgtAccountKey = Environment.GetEnvironmentVariable("TGT_ACCOUNT_KEY");
-            var tgtAccountContainer = Environment.GetEnvironmentVariable("TGT_ACCOUNT_CONTAINER");
             var tgtAccountEndpointSuffix = Environment.GetEnvironmentVariable("TGT_ACCOUNT_ENDPOINT_SUFFIX");
 
             // Information 
             Console.WriteLine($"           Account Name: {srcAccountName}");
             Console.WriteLine($"Account Endpoint-Suffix: {srcAccountEndpointSuffix}");
 
-            // instantiate the logger
-            var logger = new BackupStorageLogger();
-
-            // build a storage context 
-            using (var storageContext = new StorageContext(srcAccountName, srcAccountKey, srcAccountEndpointSuffix))
+            var operationsMode = Environment.GetEnvironmentVariable("MODE");
+            if (operationsMode != null && operationsMode.ToLower().Equals("restore"))
             {
-                // build the cloud account 
-                var backupStorageAccount = new CloudStorageAccount(new StorageCredentials(tgtAccountName, tgtAccountKey), tgtAccountEndpointSuffix, true);
+                // check the local parameters 
+                ValidateParameter("SRC_ACCOUNT_CONTAINER");
+                ValidateParameter("SRC_BACKUP_ID");
 
-                // instantiate the backup service 
-                var backupService = new BackupService(storageContext, backupStorageAccount, logger);
+                // load the local parameters
+                var srcAccountContainer = Environment.GetEnvironmentVariable("SRC_ACCOUNT_CONTAINER");
+                var srcBackupId = Environment.GetEnvironmentVariable("SRC_BACKUP_ID");
 
-                // build the backup prefix 
-                var prefix = DateTime.Now.ToString("yyyy-mm-dd") + "-" + Guid.NewGuid().ToString();
+                // log
+                Console.WriteLine($"        Operations Mode: Restore");
 
-                // exceute the backup 
-                backupService.Backup(tgtAccountContainer, prefix).Wait();
+                // instantiate the logger
+                var logger = new BackupStorageLogger();
+
+                // build a storage context 
+                using (var storageContext = new StorageContext(tgtAccountName, tgtAccountKey, tgtAccountEndpointSuffix))
+                {
+                    // build the cloud account 
+                    var backupStorageAccount = new CloudStorageAccount(new StorageCredentials(srcAccountName, srcAccountKey), srcAccountEndpointSuffix, true);
+
+                    // instantiate the backup service 
+                    var backupService = new BackupService(storageContext, backupStorageAccount, logger);
+
+                    // exceute the backup 
+                    backupService.Restore(srcAccountContainer, srcBackupId).Wait();
+                }
+
+                // Thank you 
+                Console.WriteLine("Restore is finished");
             }
+            else
+            {
+                // check the local parameters 
+                ValidateParameter("TGT_ACCOUNT_CONTAINER");
 
-            // Thank you 
-            Console.WriteLine("Backup is finished");
+                // load the local parameters
+                var tgtAccountContainer = Environment.GetEnvironmentVariable("TGT_ACCOUNT_CONTAINER");
+
+                // log
+                Console.WriteLine($"        Operations Mode: Backup");
+
+                // instantiate the logger
+                var logger = new BackupStorageLogger();
+
+                // build a storage context 
+                using (var storageContext = new StorageContext(srcAccountName, srcAccountKey, srcAccountEndpointSuffix))
+                {
+                    // build the cloud account 
+                    var backupStorageAccount = new CloudStorageAccount(new StorageCredentials(tgtAccountName, tgtAccountKey), tgtAccountEndpointSuffix, true);
+
+                    // instantiate the backup service 
+                    var backupService = new BackupService(storageContext, backupStorageAccount, logger);
+
+                    // build the backup prefix 
+                    var prefix = DateTime.Now.ToString("yyyy-mm-dd") + "-" + Guid.NewGuid().ToString();
+
+                    // log
+                    Console.WriteLine($"          Backup Prefix: {prefix}");
+
+                    // exceute the backup 
+                    backupService.Backup(tgtAccountContainer, prefix).Wait();
+                }
+
+                // Thank you 
+                Console.WriteLine("Backup is finished");
+            }
         }
 
         static void ValidateParameter(string variable) {
